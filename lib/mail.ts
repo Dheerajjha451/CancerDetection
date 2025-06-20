c9af7ec891e5407523bc816093e05ad4a25b6718
Dheerajjha451 <dheerajjha451@gmail.com>
2025-06-20 18:27:20 +0530

    Change domain

--- lib/mail.ts
+++ lib/mail.ts
@@ -6,7 +6,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 //send email for reset password
   await resend.emails.send({
-    from: "authentication@designtocode.me",
+    from: "authentication@dheerajjha.com",
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
@@ -17,7 +17,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 //send email for email verification 
   await resend.emails.send({
-    from: "authentication@designtocode.me",
+    from: "authentication@dheerajjha.com",
     to: email,
     subject: "Confirm your email",
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
